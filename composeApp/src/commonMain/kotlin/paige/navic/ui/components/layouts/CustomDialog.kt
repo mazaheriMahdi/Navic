e2c1a04bb4e760ef7cf37295bf12757b233a7400
paige <paigely@tuta.io>
2026-03-19 20:32:36 +0100

    fix: formatting

--- composeApp/src/commonMain/kotlin/paige/navic/ui/components/layouts/CustomDialog.kt
+++ composeApp/src/commonMain/kotlin/paige/navic/ui/components/layouts/CustomDialog.kt
@@ -6,14 +6,10 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -70,8 +66,7 @@ fun CustomDialog(
 				horizontalArrangement = Arrangement.End,
 				verticalAlignment = Alignment.CenterVertically
 			) {
-
-					buttons()
+				buttons()
 			}
 		}
 	}
